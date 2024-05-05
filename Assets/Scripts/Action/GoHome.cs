529f1d568978f3f615f3c3dd67f454394175e670
Shya <67426767+cungaminoan@users.noreply.github.com>
2024-05-05 16:20:48 +0700

    finished the GOAP

---
+++ Assets/Scripts/Action/GoHome.cs
@@ -0,0 +1,14 @@
+﻿public class GoHome : GAction {
+    public override bool PrePerform() {
+
+        beliefs.RemoveState("atHospital");
+        return true;
+    }
+
+    public override bool PostPerform() {
+
+        Destroy(this.gameObject, 1.0f);
+        Spawner.Instance.currentPatients--;
+        return true;
+    }
+}
---
+++ Assets/Scripts/Controllers/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private static Spawner instance;
+    public GameObject patientPrefab;
+    public int numOfPatients;
+    [SerializeField]
+    private int limitOfPatients = 10;
+    public int currentPatients;
+
+    public static Spawner Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<Spawner>();
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        for (int i = 0; i < numOfPatients; i++)
+        {
+            Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+            currentPatients++;
+        }
+        Invoke("SpawnPatient", Random.Range(12, 15));
+    }
+
+    void SpawnPatient()
+    {
+        if (currentPatients < limitOfPatients)
+        {
+            Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+            currentPatients++;
+            Invoke("SpawnPatient", Random.Range(12, 15));
+        }
+    }
+}
