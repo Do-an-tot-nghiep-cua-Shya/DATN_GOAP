e0cc9c8d7015d87d34155cce368bdf9705519257
Shya <67426767+cungaminoan@users.noreply.github.com>
2024-05-19 19:06:25 +0700

    Guard Modified

--- Assets/Scripts/BehaviourTree/GuardBehaviour.cs
+++ Assets/Scripts/BehaviourTree/GuardBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class RobberBehaviour : BTAgent
+public class GuardBehaviour : BTAgent
 {
     public GameObject door;
     public GameObject toilet;
